81ad2f50fab5e41105e799782b9273eca36bfae7
dharmeshgidwani <rajeshsonia50.rg@gmail.com>
2024-06-12 00:20:02 +0530

    update

--- Components/HomePage.js
+++ Components/HomePage.js
@@ -7,7 +7,7 @@ const HomePage = () => {
     <div className='flex flex-col justify-center items-center'>
       <Confetti />
       <h1 className='uppercase text-red-500 text-6xl md:text-9xl lg:text-10xl mb-5 text-center'>
-        babezz ka birthday
+        ABCD
       </h1>
       {/* <button className='bg-white text-pink-500 rounded-full shadow-md hover:bg-gray-100 p-3 md:p-4 lg:p-5 text-sm md:text-base lg:text-lg'>
         Click here
