b6c9fd481c703c3cf1f06c2ae41fbd973b521673
dharmeshgidwani <rajeshsonia50.rg@gmail.com>
2024-06-12 00:31:11 +0530

    Updated

--- app/page.js
+++ app/page.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import Link from 'next/link'
-import HomePage from '@/Components/HomePage.js'
+import Confetti from '@/Components/Confetti'
 
 const page = () => {
 
   return (
     <div className='bg-pink-200 min-h-screen w-full p-5 flex flex-col justify-center items-center'>
-      <HomePage />
-      
+      <Confetti />
+      <h1 className='uppercase text-red-500 text-6xl md:text-9xl lg:text-10xl mb-5 text-center'>
+        babezz ka birthday
+      </h1>
+      {/* <button className='bg-white text-pink-500 rounded-full shadow-md hover:bg-gray-100 p-3 md:p-4 lg:p-5 text-sm md:text-base lg:text-lg'>
+        Click here
+      </button> */}
+      <Link className=' bg-white text-pink-500 rounded-full shadow-md hover:bg-gray-100 p-3 md:p-4 lg:p-5 text-sm md:text-base lg:text-lg' href="/MorePage">Here</Link>
     </div>
   )
 }
