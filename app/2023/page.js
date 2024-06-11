ca37bdb6499f4fa4351795a1d13ea006665a44f7
dharmeshgidwani <rajeshsonia50.rg@gmail.com>
2024-06-12 00:17:01 +0530

    project updates

--- app/2023/page.js
+++ app/2023/page.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Confetti from '@/Components/Confetti'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='bg-pink-200 min-h-screen w-full p-5 flex flex-col justify-center items-center'>
+        <Confetti />
+        <p>Happy Birthday Babezz💛🐷🦛I know i don’t say it enough aur enough bhi kya kabhi hii ni bolti hamesha tu mujhe bolta hai par babezz tu bhi best hai yaar. From being the chhole to my bhature, my personal photographer and my tutor tu sab hii krleta hai sahi bolti hu mai perfect hai tu. Mere bags aur purse se leke drame sab utha leta h tu. par khud bhi boht krta hai. Mujhe rulata bhi tu hai aur chup bhi tuhi krata h aur fr mujhe dekh k khud bhi rota hai. Gussa bhi chadata h fr mood bhi khud theek krta h. Bas monday ko apna mood ni theek kr pata. If there is one person i can be however i am with, is you. Gande se gande jokes(jo tere se seekha hai) sunane se, nayi nayi khane ki jagah discover krna,gandi se gandi movies dekhna,100 adjectives se ek dusre ko bulana, diljit ka har gana har movie pe vibe krna aur kon krta ye mere saath. Merse itne thappad khana aur irritate krna is my love language ko smjhna ab har kisi k bass ki baat thodi hai. Log chahe sau baar judge krte ho hamko but they’ll never get us kioki unke pass tere jaisa dost nahi h na yaar na mai milne dungi kisi ko. If not you i wouldn’t want it any way else. I never knew friendships like this existed until u showed it to me. I can’t imagine a life without you in it(jgah hii itni leta h). I wish i had found you earlier toh ab hamari non decreasing dosti aur bhi achi hoti.  Since you make me the happiest toh Happiest 5.2nd birthday to you babezz!!! Love you💛. And no distance, condition or argument will ever change what you mean to me.</p>
+    </div>
+
   )
 }
 
---
+++ app/2024/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-pink-200 min-h-screen w-full p-5 flex flex-col justify-center items-center'>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium labore, vero assumenda unde nihil amet mollitia voluptates quisquam omnis numquam soluta vel quo id dolores quos, doloremque voluptatem dolore dignissimos.</p>
+    </div>
+  )
+}
+
+export default page
