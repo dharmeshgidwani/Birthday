f814d93e61483723ac21162849d2392c13409196
dharmeshgidwani <rajeshsonia50.rg@gmail.com>
2024-06-12 14:03:25 +0530

    Photos updated

--- app/MorePage/page.js
+++ app/MorePage/page.js
@@ -2,15 +2,30 @@ import Confetti from '@/Components/Confetti'
 import Link from 'next/link'
 import React from 'react'
 
-const page = () => {
+const Page = () => {
   return (
     <div className='bg-pink-200 min-h-screen w-full p-5 flex flex-col'>
-        <ul className='list-disc'>
-          <li><Link href="/2023">2023</Link></li>
-          <li><Link href="/2024">2024</Link></li>
-        </ul>
+      <Confetti />
+      <ul className='list-disc text-2xl mb-10'>
+        <li><Link href="/2023">2023</Link></li>
+        <li><Link href="/2024">2024</Link></li>
+      </ul>
+      <div className='flex flex-wrap gap-4 justify-center'>
+        <div className='w-[100px] h-[100px] sm:w-[150px] sm:h-[150px] md:w-[200px] md:h-[200px] lg:w-[250px] lg:h-[250px] xl:w-[300px] xl:h-[300px] bg-green-100'>
+          <img className='w-full h-full object-cover' src="../images/47293.JPG" alt="Hey" />
+        </div>
+        <div className='w-[100px] h-[100px] sm:w-[150px] sm:h-[150px] md:w-[200px] md:h-[200px] lg:w-[250px] lg:h-[250px] xl:w-[300px] xl:h-[300px] bg-green-100'>
+          <img className='w-full h-full object-cover' src="../images/47308.JPG" alt="Hey" />
+        </div>
+        <div className='w-[100px] h-[100px] sm:w-[150px] sm:h-[150px] md:w-[200px] md:h-[200px] lg:w-[250px] lg:h-[250px] xl:w-[300px] xl:h-[300px] bg-green-100'>
+          <img className='w-full h-full object-cover' src="../images/47678.JPG" alt="Hey" />
+        </div>
+        <div className='w-[100px] h-[100px] sm:w-[150px] sm:h-[150px] md:w-[200px] md:h-[200px] lg:w-[250px] lg:h-[250px] xl:w-[300px] xl:h-[300px] bg-green-100'>
+          <img className='w-full h-full object-cover' src="../images/52931.JPG" alt="Hey" />
+        </div>
+      </div>
     </div>
   )
 }
 
-export default page
+export default Page
--- app/page.js
+++ app/page.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import Link from 'next/link'
 import Confetti from '@/Components/Confetti'
+import { motion } from 'framer-motion'
 
 const page = () => {
 
   return (
-    <div className='bg-pink-200 min-h-screen w-full p-5 flex flex-col justify-center items-center'>
-      <Confetti />
-      <h1 className='uppercase text-red-500 text-6xl md:text-9xl lg:text-10xl mb-5 text-center'>
-        babezz ka birthday
-      </h1>
-      {/* <button className='bg-white text-pink-500 rounded-full shadow-md hover:bg-gray-100 p-3 md:p-4 lg:p-5 text-sm md:text-base lg:text-lg'>
-        Click here
-      </button> */}
-      <Link className=' bg-white text-pink-500 rounded-full shadow-md hover:bg-gray-100 p-3 md:p-4 lg:p-5 text-sm md:text-base lg:text-lg' href="/MorePage">Here</Link>
-    </div>
+    <>
+      <div className='bg-pink-200 min-h-screen w-full p-5 flex flex-col justify-center items-center'>
+        <marquee className='absolute top-0 left-0 w-full p-2 bg-pink-300 text-center text-5xl uppercase text-white'>
+         <h1>Love you babezz💛 Love you babezz💛 Love you babezz💛 Love you babezz💛</h1>
+        </marquee>
+        <Confetti />
+        <h1 className='uppercase text-red-500 text-6xl md:text-9xl lg:text-10xl mb-5 text-center'>
+          babezz ka birthday
+        </h1>
+        {/* <button className='bg-white text-pink-500 rounded-full shadow-md hover:bg-gray-100 p-3 md:p-4 lg:p-5 text-sm md:text-base lg:text-lg'>
+          Click here
+        </button> */}
+        <Link className=' bg-white text-pink-500 rounded-full shadow-md hover:bg-gray-100 p-3 md:p-4 lg:p-5 text-sm md:text-base lg:text-lg' href="/MorePage">Click Here</Link>
+        <marquee className='absolute bottom-0 left-0 w-full p-2 bg-pink-300 text-center text-5xl uppercase text-white'>
+         <h1>Love you babezz💛 Love you babezz💛 Love you babezz💛 Love you babezz💛</h1>
+        </marquee>
+      </div>
+    </>
+    
   )
 }
 
